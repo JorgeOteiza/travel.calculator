87c062ba42ca54bf365f5587cad0397ec2731eed
JorgeOteiza <oteiza.jor@gmail.com>
2025-01-04 14:56:57 -0300

    configuración .env

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+// Renderizar la aplicación principal
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+export { GOOGLE_MAPS_API_KEY, BACKEND_URL };
